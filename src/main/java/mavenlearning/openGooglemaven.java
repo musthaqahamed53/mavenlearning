bfd02b743dea632193cd7b79951c1383ef7db4c9
musthaqahamed53 <musthaq53ahamed@gmail.com>
2022-06-26 01:54:24 +0530

    update

--- src/main/java/mavenlearning/openGooglemaven.java
+++ src/main/java/mavenlearning/openGooglemaven.java
@@ -4,16 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class openGooglemaven {
 
 	 static WebDriver driver; 
 	 public static void main(String[] args) {
-	    System.setProperty("webdriver.gecko.driver", "D:\\CTS\\geckodriver.exe");
+		 ExtentSparkReporter reporter = new ExtentSparkReporter("report.html");
+		 ExtentReports extentReports = new ExtentReports();
+		 extentReports.attachReporter(reporter);
+		 ExtentTest test=extentReports.createTest("Whatsup Duck");
+		 
+		 
+		 System.setProperty("webdriver.gecko.driver", "D:\\CTS\\geckodriver.exe");
 	    driver= new FirefoxDriver();
 	    driver.get("http://www.google.com");
 	    driver.findElement(By.name("q")).sendKeys("Whatsup Duck!");
 	    driver.findElement(By.name("q")).submit();
 	   // driver.quit();
+	    test.pass("Test Passed");
+	    extentReports.flush();
 	}
 
 }
